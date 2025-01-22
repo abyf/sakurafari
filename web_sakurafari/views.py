342dd34ab30edbf9728db77c04842c38730a8656
abyf <abyf2012@yahoo.fr>
2025-01-22 22:54:48 +0000

    Added about-Us, contact html pages

--- web_sakurafari/views.py
+++ web_sakurafari/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html',{})
+
+def contact(request):
+    return render(request, 'contact.html',{})
+
+def about_us(request):
+    return render(request, 'about-us.html',{})
